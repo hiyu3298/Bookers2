90d72d26ed16ba261c8c7ac75df78d6c37df3971
hiyu3298 <akamatsu11218991@gmail.com>
2021-02-27 06:03:24 +0000

    [Add] ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root "homes#top"
+
+  resource :users
 end
